124a7a9a79ca3225c909d551802259b39ab6b93d
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-11-05 06:43:11 +0900

    feat: pretty progress display

--- reversi_ai/train.py
+++ reversi_ai/train.py
@@ -25,6 +25,28 @@ def train_loop(model, dataloader, loss_fn, optimizer) -> None:
         optimizer.step()
 
 
+def show_progress(
+    title: str,
+    n_finished: int,
+    n_target: int,
+    length: int = 20,
+    done: bool = False,
+):
+    l_finished = n_finished * length // n_target
+    if done:
+        print(
+            f"\r{title} |{'='*(length+1)}|",
+            f"{n_target} / {n_target} (100.0%)",
+            end="\n",
+        )
+    else:
+        print(
+            f"\r{title} |{'='*l_finished}>{' '*(length-l_finished)}|",
+            f"{n_finished} / {n_target} ({n_finished/n_target*100:.1f}%)",
+            end="",
+        )
+
+
 def main(state_dict_path: str = None):
     ffn = FFN()
 
@@ -34,7 +56,7 @@ def main(state_dict_path: str = None):
     ffn.train()
 
     for epoch in itertools.count():
-        print(f'epoch {epoch}')
+        print(f"epoch {epoch}")
         # ======== データセットの生成 ========
         n_games = 100
         n_max_hands = 60
@@ -50,7 +72,7 @@ def main(state_dict_path: str = None):
         flat_coord_to_rc_index = np.mgrid[0:8, 0:8].reshape((2, -1))
 
         for game_i in range(n_games):
-            print(f"\rgame {game_i} / {n_games}", end="")
+            show_progress("game", game_i, n_games)
             reversi = Reversi(8, 8)
             history_i = 0
             history_x = np.empty((n_max_hands, 8 * 8), dtype=np.float32)
@@ -77,7 +99,9 @@ def main(state_dict_path: str = None):
                     evaluation_value[placeable_index].detach().numpy()
                 )
                 chosen_index = np.random.choice(placeable_index, p=probability)
-                chosen_row, chosen_column = flat_coord_to_rc_index[:, chosen_index]
+                chosen_row, chosen_column = flat_coord_to_rc_index[
+                    :, chosen_index
+                ]
 
                 # 手番の仮保存
                 history_x[history_i] = reversi.board.flatten()
@@ -103,9 +127,9 @@ def main(state_dict_path: str = None):
             train_x[train_i : train_i + history_i] = history_x[  # noqa: E203
                 :history_i
             ]
-            train_turn[train_i : train_i + history_i] = history_turn[  # noqa: E203
-                :history_i
-            ]
+            train_turn[
+                train_i : train_i + history_i  # noqa: E203
+            ] = history_turn[:history_i]
             train_y[train_i : train_i + history_i] = np.array(  # noqa: E203
                 [
                     label_to_onehot[hand]
@@ -121,6 +145,8 @@ def main(state_dict_path: str = None):
         train_x.resize((train_i, 8 * 8))
         train_turn.resize((train_i,))
         train_y.resize((train_i, 8 * 8))
+
+        show_progress("game", n_games, n_games, done=True)
 
         # ======== DataLoader の設定 ========
         train_x = torch.tensor(train_x, dtype=torch.float)
@@ -138,9 +164,10 @@ def main(state_dict_path: str = None):
         train_loop(ffn, data_loader, loss_fn, optimizer)
 
         # ======== モデルの保存 ========
+        timestamp = datetime.datetime.utcnow().timestamp()
         torch.save(
             ffn.state_dict(),
-            f"reversi_ffn_model-{datetime.datetime.utcnow().timestamp()}.pickle",
+            f"reversi_ffn_model-{timestamp}.pickle",
         )
 
 
